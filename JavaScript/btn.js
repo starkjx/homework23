e0ac4013bc1fef8b245c192e4f7b5f7028a03dd5
starkjx <303245803@qq.com>
2018-12-15 20:36:57 +0800

    add

--- JavaScript/btn.js
+++ JavaScript/btn.js
@@ -1,5 +1,7 @@
 var addBtn = document.querySelector('#addbtn');
 var removeBtn = document.querySelector('#removebtn');
+var toggle1Btn =document.querySelector('#toggle1');
+var toggle2Btn =document.querySelector('#toggle2');
 var boxPanel = document.querySelector('.box');
 
 addBtn.addEventListener('click',function(){
@@ -9,4 +11,16 @@ addBtn.addEventListener('click',function(){
 
 removeBtn.addEventListener('click',function(){
   boxPanel.classList.remove('active');
+});
+
+toggle1Btn.addEventListener('click',function(){
+  if(boxPanel.classList.contains('active')){
+    boxPanel.classList.remove('active');
+  }else{
+    boxPanel.classList.add('active');
+  }
+});
+
+toggle2Btn.addEventListener('click',function(){
+  boxPanel.classList.toggle('active');
 });
